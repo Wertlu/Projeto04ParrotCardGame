d9539662e7a4acf9c3bb33b0d1b054b294b92b27
Wertlu <luiz_felipe_js@hotmail.com>
2021-11-08 16:56:45 -0300

    Last update

--- script.js
+++ script.js
@@ -24,15 +24,40 @@ function startGame() {
         gameCards.push(randCardsFront[i]);
     }
     let randGameCards = gameCards.sort(() => Math.random() - 0.5);
-    console.log(randGameCards);
     for (let i = 0; i < cardsN; i++) {
         document.querySelector("ul").innerHTML += `<li class="card" onclick="cardSelection(this)"><div class="back-card"><img src="assets/back.png"></div><div class="front-card"><img src="${randGameCards[i]}"></div></li>`;
     }
 }
+let selectedCards = [];
 
 function cardSelection(card) {
-    if (card.classList.value === "card") {
-        card.classList.add(".selected1");
-        console.log(card.classList);
+    card.classList.add("selected1");
+    selectedCards.push(card.lastChild.innerHTML);
+    if (selectedCards.length === 2) {
+        console.log(selectedCards[0].innerHTML);
+        if (selectedCards[0].innerHTML === selectedCards[1].innerHTML) {
+            selectedCards[0].classList.remove("selected1");
+            selectedCards[1].classList.remove("selected1");
+            selectedCards[0].classList.add("done");
+            selectedCards[1].classList.add("done");
+            selectedCards = [];
+        } else {
+            setTimeout(diferentCards, 1500);
+        }
     }
+    let done = document.querySelectorAll(".done");
+    let allCards = document.querySelectorAll(".card")
+    if (done.length === allCards.length) {
+        setTimeout(gameover, 500);
+    }
+}
+
+function diferentCards() {
+    selectedCards[0].classList.remove("selected1");
+    selectedCards[1].classList.remove("selected1");
+    selectedCards = [];
+}
+
+function gameOver() {
+    alert("Acabou!")
 }
